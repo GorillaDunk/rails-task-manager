37592706a774fc6af88974f9df9f30110f4a2288
TomKim <ehdgus5289@gmail.com>
2023-05-23 16:55:19 -0400

    destroy completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -30,8 +30,9 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(param[:id])
+    @task = Task.find(params[:id])
     @task.destroy
+    redirect_to tasks_path
   end
 
   private
